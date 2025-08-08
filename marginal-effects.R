093f58f819eb19a68c8be8b6f13806046a59ad58
Shaun Kellogg <sbkellog@ncsu.edu>
2025-08-08 14:03:32 -0400

    added r script in explorer

--- marginal-effects.R
+++ marginal-effects.R
@@ -1,3 +1,9 @@
 # load packages ---------------------------------------------------------------
 library(tidyverse)
 library(marginaleffects)
+
+
+# Load the dataset -----------------------------------------------------------
+batting = get_dataset("Batting", package = "Lahman") |>
+  select(playerID, yearID, teamID, lgID, AB, HR) |>
+  filter(yearID >= 2000, yearID <= 2010, AB >= 100)
